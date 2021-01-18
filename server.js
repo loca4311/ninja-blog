60da5f294fed14a0f5dbae62aaf9c4bfe620c885
Anatoliy <andyone4311@gmail.com>
2021-01-18 18:07:57 +0200

    update requests & responses

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // send am HTML file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+
 });
+// 13:23
 
 server.listen(3000, 'localhost', () => {
     console.log('listening for request on port 3000');
