f19e69e6629e9fc29919fe088502d85ea27fb9b1
Anatoliy <andyone4311@gmai.com>
2021-01-18 22:13:18 +0200

    update lesson 2

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const http = require('http');
 const fs = require('fs');
 
@@ -7,8 +12,29 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+    
     // send am HTML file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err);
             res.end();
